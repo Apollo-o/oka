8e178677200922605c0e94e21a6fe38153f1570e
o-o <22546578+Apollo-o@users.noreply.github.com>
2025-03-24 10:13:28 -0700

    Add files via upload
    Signed-off-by: o-o <22546578+Apollo-o@users.noreply.github.com>

--- dependencies/make.cmd
+++ dependencies/make.cmd
@@ -1,8 +1,10 @@
+@echo off
 SET path=%~dp0
 SET exif="%path%exiftool_files.7z"
 SET ffmpeg="%path%ffmpeg.7z"
-tar -xf %exif%
-tar -xf %ffmpeg%
+SET root=%SystemRoot%\System32\
+%root%tar.exe -xf %exif%
+%root%tar.exe -xf %ffmpeg%
+%root%timeout.exe /t 15 /nobreak >nul
 del %exif%
 del %ffmpeg%
-pip install pillow ImageHash ffmpeg-python opencv-python --user
