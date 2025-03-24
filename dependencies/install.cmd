a75c1c3c0975504b28997438db75abe4a922391c
o-o <22546578+Apollo-o@users.noreply.github.com>
2025-03-23 22:01:23 -0700

    Add files via upload

--- dependencies/install.cmd
+++ dependencies/install.cmd
@@ -1,1 +1,8 @@
+SET path=%~dp0
+SET exif="%path%exiftool_files.7z"
+SET ffmpeg="%path%ffmpeg.7z"
+tar -xf %exif%
+tar -xf %ffmpeg%
+del %exif%
+del %ffmpeg%
 pip install ffmpeg-python opencv-python --user; cls
